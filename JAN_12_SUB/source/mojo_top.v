b3b0d3983355245dbc00e1dde6c6aa9f277c4772
delkovebay <delkovebay@gmail.com>
2018-01-14 21:57:11 +0900

    params is done

---
+++ JAN_12_SUB/source/mojo_top.v
@@ -0,0 +1,302 @@
+module mojo_top(
+
+  // GLOBAL
+  input clk, // 50Mhz
+  input rst_n,
+  input SERIAL_IN,
+
+  output ENABLE,
+  
+  // AVR 
+  input cclk,
+  input spi_ss,
+  input spi_mosi,
+  input spi_sck,
+  input avr_tx, // AVR Tx => FPGA Rx
+  input avr_rx_busy, // AVR Rx buffer full
+
+  output spi_miso,
+  output [3:0] spi_channel,
+  output avr_rx, // AVR Rx => FPGA Tx
+
+  // TDC
+  input TDC_INTB,
+  input TDC_DOUT,
+
+  output TDC_MOSI,
+  output TDC_SPI_CLOCK,
+  output TDC_CS,
+  output TDC_REF_CLOCK,
+  output TDC_START_SIGNAL,
+  output SERIAL_OUT_TDC,
+
+ // MEMS
+  output MEMS_MOSI,
+  output MEMS_SPI_CLOCK,
+  output MEMS_CS,
+  output MEMS_FCLK
+
+  // DEBUGGING
+  // input TDC_TRIG,
+    // FIFO
+  // output FIFO_FULL,
+  // output FIFO_EMPTY,
+  // output w_rd_en,
+  // output w_wr_en,
+    // SERIAL
+  // output tx_busy_TDC,
+  // output t_new_data_FROM_FIFO_TO_SERIAL
+  // output play,
+  // output pause,
+  );
+ 
+  // PARAMS
+  localparam MOJO_FREQUENCY=50000000; // constant
+  // must be 50/2^n, since they are clock, otherwise will be rounded to closest minimum one. e.g 3.3kk->3.125kk
+  // TDC
+  localparam TDC_REF_CLOCK_FREQUENCY=12500000; // 12500000, range [1-16MHz]
+  localparam TDC_SPI_SPEED=6250000; // 6250000, max is  20Mhz
+  // MEMS
+  localparam FCLK_FREQUENCY=10000; // 10k -> 6k
+  localparam MEMS_SPI_SPEED=25000;  // max is 50MHZ
+  // FIFO
+  localparam FIFO_WIDTH=5; // means count of elements is 2^5
+  localparam BAUD_RATE_SPEED=4000000; // in Hz, CAN BE ANY
+
+  // OTHER
+  localparam SHOOTING_FREQUENCY=100; // in Hz, CAN BE ANY
+    
+  // CONVERTED PARAMS  
+  localparam FCLK_FREQUENCY_PARAM=MOJO_FREQUENCY/FCLK_FREQUENCY; 
+  localparam SHOOTING_PARAM=MOJO_FREQUENCY/SHOOTING_FREQUENCY;
+  localparam BAUD_RATE_PARAM=MOJO_FREQUENCY/BAUD_RATE_SPEED;
+  localparam TDC_REF_PARAM=MOJO_FREQUENCY/TDC_REF_CLOCK_FREQUENCY; 
+  
+  localparam TDC_SPI_SPEED_PARAM= MOJO_FREQUENCY/TDC_SPI_SPEED; 
+  localparam MEMS_SPI_SPEED_PARAM= MOJO_FREQUENCY/MEMS_SPI_SPEED; 
+
+
+  // GLOBAL
+  wire rst = ~rst_n; // make reset active high
+  wire soft_reset;
+  wire ENABLE; // enable both for TDC & MEMS
+  wire pause; // to control play/pause
+
+  // MY AVR
+  wire [7:0] tx_data;
+  wire new_tx_data;
+  wire tx_busy;
+
+  // EXTERNAL SERIAL
+  // wire [7:0] rx_data2;
+  // wire new_rx_data2;
+  
+  wire [7:0] rx_data;
+  wire new_rx_data;
+  
+
+  // MEMS
+  wire mems_SPI_start;
+  // wire mems_SPI_new_data; // debugging
+  wire mems_SPI_busy;
+  wire [23:0] data_miso;
+  
+  // TDC-SPI
+  wire tdc_SPI_start;
+  // wire tdc_SPI_new_data; // debugging
+  wire [7:0] tdc_data_in;
+  wire tdc_SPI_busy;
+  wire [7:0] tdc_data_out;
+  wire CS_END;
+
+  // FIFO
+  wire new_line_FIFO_done;
+  wire new_frame_FIFO_done;
+  wire new_line;
+  wire new_frame;
+
+  wire s1_wr_en;
+  wire [47:0] s1_din;
+  wire s1_fifo_writing_done;
+  // wire s2_fifo_writing_done;
+
+  // assign w_rd_en = t_rd_en;
+  // assign w_wr_en = s1_wr_en;
+
+  avr_interface avr_interface (
+    .clk(clk),
+    .rst(rst),
+    .cclk(cclk),
+    .spi_miso(spi_miso),
+    .spi_mosi(spi_mosi),
+    .spi_sck(spi_sck),
+    .spi_ss(spi_ss),
+    .spi_channel(spi_channel),
+    .tx(avr_rx), // FPGA tx goes to AVR rx
+    .rx(avr_tx),
+    .channel(4'd15), // invalid channel disables the ADC
+    .new_sample(),
+    .sample(),
+    .sample_channel(),
+    .tx_data(tx_data),
+    .new_tx_data(new_tx_data),
+    .tx_busy(tx_busy),
+    .tx_block(avr_rx_busy),
+    .rx_data(rx_data),
+    .new_rx_data(new_rx_data)
+  );
+ 
+  main_control main_control (
+    // INPUT
+    .clk(clk),
+    .rst(rst),
+    .rx_data(rx_data), // serial for communication
+    .new_rx_data(new_rx_data), // serial for communication
+    
+    // OUTPUT
+    .tdc_enable(ENABLE), // using from low to high
+    .soft_reset(soft_reset),
+    .pause(pause) // global pause
+  );
+
+
+  tdc_control #(.SHOOTING_PARAM(SHOOTING_PARAM)) tdc_control (
+    // INPUT
+    .clk(clk),
+    .rst(rst),
+    // .tdc_SPI_new_data(tdc_SPI_new_data),
+    .tdc_MISO(tdc_data_out),
+    .tdc_soft_reset(soft_reset),
+    .TDC_INTB(TDC_INTB),
+    .tdc_SPI_busy(tdc_SPI_busy),
+    .fifo_writing_done(s1_fifo_writing_done),
+    .pause(pause),
+
+    // OUTPUT
+    .start_signal(TDC_START_SIGNAL),
+    .CS_END(CS_END),
+    .start(tdc_SPI_start),
+    .tdc_MOSI(tdc_data_in),
+    .w_wr_en(s1_wr_en),
+    // .w_wr_en2(s2_wr_en),
+    .data_TO_FIFO(s1_din)
+    // .fifo_writing_done2(s2_fifo_writing_done),
+    // .laser_trig(laser_trig)
+  );
+ 
+  
+  fifo_manager #(.BAUD_RATE_PARAM(BAUD_RATE_PARAM), .FIFO_WIDTH(FIFO_WIDTH)) fifo_manager (
+    // INPUT
+    .clk(clk),
+    .rst(rst),
+    .s1_wr_en(s1_wr_en),
+    .s2_wr_en(s2_wr_en),
+    .s1_din(s1_din),
+    .new_line(new_line),
+    .new_frame(new_frame),
+    
+    // OUTPUT
+    .new_line_FIFO_done(new_line_FIFO_done),
+    .new_frame_FIFO_done(new_frame_FIFO_done),
+
+    .w_s1_fifo_writing_done(s1_fifo_writing_done),
+    // .w_s2_fifo_writing_done(s2_fifo_writing_done),
+    .tx_busy_TDC(tx_busy_TDC),
+    .new_data_FROM_FIFO_TO_SERIAL(t_new_data_FROM_FIFO_TO_SERIAL),
+    .w_tx_OUT_TDC(SERIAL_OUT_TDC),
+    // debug
+    .w_empty(FIFO_EMPTY),
+    .w_full(FIFO_FULL),
+    .t_rd_en(t_rd_en)
+  );
+
+
+  // 3 for 6 MHz
+  tdc_spi_master #(.CLK_DIV(TDC_SPI_SPEED_PARAM)) tdc_spi_master(
+    // INPUT
+    .clk(clk),
+    .rst(rst),
+    .miso(TDC_DOUT),
+    .start(tdc_SPI_start), // for SPI
+    .data_in(tdc_data_in),
+    .CS_END(CS_END), // manage up/down real CS pin -- TDC_CS
+    
+    // OUTPUT
+    .mosi(TDC_MOSI),
+    .sck(TDC_SPI_CLOCK),
+    .data_out(tdc_data_out),
+    .busy(tdc_SPI_busy),
+    .new_data(tdc_SPI_new_data),
+    .CS(TDC_CS)
+  );
+   
+  
+  mems_control mems_control (
+    // INPUT
+    .clk(clk),
+    .rst(rst),
+    .pause(pause),
+    .mems_SPI_busy(mems_SPI_busy),
+    .mems_soft_reset(soft_reset),
+    
+    .new_line_FIFO_done(new_line_FIFO_done),
+    .new_frame_FIFO_done(new_frame_FIFO_done),
+
+    // OUTPUT
+    .mems_SPI_start(mems_SPI_start),
+    .data_miso(data_miso),
+    .new_frame(new_frame),
+    .new_line(new_line)
+  );
+
+
+  // 7 is 16 ms, 10 - 140ms, 4 is 2ms
+  mems_spi #(.CLK_DIV(MEMS_SPI_SPEED_PARAM)) mems_spi_master(
+    // INPUT
+    .clk(clk),
+    .rst(rst),
+    .miso(miso),
+    .data_in(data_miso),
+    .start(mems_SPI_start),
+
+    // OUTPUT
+    .mosi(MEMS_MOSI),
+    .sck(MEMS_SPI_CLOCK),
+    .data_out(data_out),
+    .busy(mems_SPI_busy),
+    .new_data(mems_SPI_new_data),
+    .CS(MEMS_CS)
+  );
+
+  // TDC REF clock, 12.5 MHz
+  my_clk #(.CLK_DIV(TDC_REF_PARAM)) tdc_ref_clk (
+   // INPUT
+   .clk(clk),
+   .rst(rst),
+
+   // OUTPUT
+   .my_clk(TDC_REF_CLOCK)
+  ); 
+
+  // MEMS FCLK, 10 KHz
+  my_clk #(.CLK_DIV(FCLK_FREQUENCY_PARAM)) FCLK (
+   // INPUT
+   .clk(clk),
+   .rst(rst),
+
+   // OUTPUT
+   .my_clk(MEMS_FCLK)
+  ); 
+
+
+  // UNCOMMENT TO MAKE COMMUNICATIONS VIA SERIAL
+  // serial_rx #(.CLK_PER_BIT(17)) serial_rx2 (
+  //   .clk(clk),
+  //   .rst(n_rdy),
+  //   .rx(SERIAL_IN),
+
+  //   .data(rx_data2),
+  //   .new_data(new_rx_data2)
+  // );
+
+endmodule
---
+++ JAN_12_SUB/work/planAhead/JAN_12_SUB/JAN_12_SUB.srcs/sources_1/imports/verilog/mems_control_6.v
@@ -0,0 +1,223 @@
+module mems_control_6 (
+    // INPUT
+    input clk,
+    input rst,
+    input pause,
+    input mems_SPI_busy,
+    input mems_soft_reset,
+    input new_frame_FIFO_done,
+    input new_line_FIFO_done,
+    // OUTPUT
+    output mems_SPI_start,
+    output [23:0] data_miso,
+    output reg new_frame,
+    output reg new_line
+  );
+ 
+  localparam STATE_SIZE = 2,
+  IDLE = 2'd0,
+  SOFTWARE_RESET = 2'd1,
+  LDAC_SETUP = 2'd2,
+  SET_CHANNEL = 2'd3;
+ 
+  localparam CH_STATE_SIZE = 2,
+  CH_A = 2'd0,
+  CH_B = 2'd1,
+  CH_C = 2'd2,
+  CH_D = 2'd3;
+
+  // FOR 1 AXES
+  localparam UP_LIMIT = 8'd161; // max is 140, Pk-Pk 129, Avg 79
+  localparam DOWN_LIMIT = 8'd21;
+
+  // FOR 2 AXES
+  localparam UP_LIMIT_2 = 8'd161; // 
+  localparam DOWN_LIMIT_2 = 8'd21;
+
+  reg [STATE_SIZE-1:0] state_d, state_q;
+  reg [CH_STATE_SIZE-1:0] ch_state_d, ch_state_q;
+ 
+  reg [3:0] addr_d, addr_q;
+  reg start_d, start_q; 
+  reg [7:0] delta_A_d;
+  reg [7:0] delta_A_q = DOWN_LIMIT;
+  reg [7:0] delta_B_d;
+  reg [7:0] delta_B_q = UP_LIMIT;
+  reg [7:0] delta_C_d;
+  reg [7:0] delta_C_q = DOWN_LIMIT_2;
+  reg [7:0] delta_D_d;
+  reg [7:0] delta_D_q = UP_LIMIT_2;
+
+  // reg [29:0] delay_cntr_d;
+  // reg [29:0] delay_cntr_q = 10'd1;
+
+  assign mems_SPI_start=start_q;
+
+  mems_rom_17 mems_rom (
+  .clk(clk),
+  .addr(addr_q),
+  .delta_A(delta_A_q),
+  .delta_B(delta_B_q),
+  .delta_C(delta_C_q),
+  .delta_D(delta_D_q),
+  .data(data_miso)
+  );  
+ 
+  always @(*) begin
+
+
+    if (new_line_FIFO_done==1'b1) begin
+      new_line=1'b0; // latch here is fine
+    end 
+
+
+    if (new_frame_FIFO_done==1'b1) begin
+      new_frame=1'b0; // latch here is fine
+    end 
+
+    state_d = state_q; // default values
+    addr_d = addr_q;   // needed to prevent latches
+    ch_state_d = ch_state_q;
+    delta_A_d = delta_A_q;
+    delta_B_d = delta_B_q;
+    delta_C_d = delta_C_q;
+    delta_D_d = delta_D_q;
+    // delay_cntr_d = delay_cntr_q;
+
+    case (state_q)
+      IDLE: begin
+        addr_d = 4'b0;
+        start_d=1'b0;
+        if (mems_soft_reset) begin
+          state_d = SOFTWARE_RESET;
+          start_d=1'b1;
+        end
+      end
+
+      SOFTWARE_RESET: begin
+        start_d=1'b0;
+        if (!mems_SPI_busy && start_q == 1'b0) begin 
+          addr_d=addr_q+1'b1;
+          state_d = LDAC_SETUP;
+          start_d = 1'b1;
+        end
+      end
+
+      LDAC_SETUP: begin
+        start_d=1'b0;
+        if (!mems_SPI_busy && start_q == 1'b0) begin 
+          addr_d = addr_q + 1'b1;
+          state_d = SET_CHANNEL;
+          start_d = 1'b1;
+          ch_state_d = CH_A;
+          // delay_cntr_d = 4'd1;
+        end
+      end
+
+      SET_CHANNEL: begin
+             case (ch_state_q)
+               CH_A: begin
+                start_d = 1'b0;
+
+                if ((!mems_SPI_busy && start_q == 1'b0) && pause==1'b0) begin 
+                        if (delta_A_q > UP_LIMIT) begin // new_line
+                          delta_A_d = DOWN_LIMIT;
+                          delta_B_d = UP_LIMIT;
+
+
+
+
+
+
+
+                         // WE CAN WRITE C,D only after A>UP, so no each time..S
+                          if (delta_C_q > UP_LIMIT_2) begin // new_frame
+                            delta_C_d = DOWN_LIMIT_2;
+                            delta_D_d = UP_LIMIT_2;  
+
+                            new_frame=1'b1;
+
+                          end else begin
+                            new_line=1'b1; // sent it obly if not frame! so new frame starts fine
+                            // change speed of SAW, CAREFUL! should nt be NEGATIVE
+                            delta_C_d = delta_C_d + 6'd1; 
+                            delta_D_d = delta_D_d - 6'd1;
+                          end // new_frame
+                           
+                        end else begin
+                          delta_A_d = delta_A_q + 1'b1;
+                          delta_B_d = delta_B_q - 1'b1;
+                        end // new_line
+
+
+                       addr_d = 4'd3;  
+                       start_d = 1'b1;
+                       ch_state_d = CH_B; 
+                end // new data
+
+                    // // IF DELAY IS NEEDED CHANGE VALUE OR JUST CHANGE SPI SPEED (PREFER)
+                    // if (delay_cntr_q==29'd50000) begin
+                    //    addr_d = 4'd3;  
+                    //    start_d = 1'b1;
+                    //    ch_state_d = CH_B; 
+                    // end else begin
+                    //    delay_cntr_d = delay_cntr_q + 1'b1;
+                    // end
+               end
+
+               CH_B: begin
+                 start_d=1'b0;
+                 if (!mems_SPI_busy && start_q == 1'b0) begin 
+                   addr_d = 4'd4;
+                   start_d = 1'b1;
+                   ch_state_d = CH_C; 
+                 end
+               end
+
+               CH_C: begin
+                 start_d=1'b0;
+                 if (!mems_SPI_busy && start_q == 1'b0) begin 
+                   addr_d = 4'd5;
+                   start_d = 1'b1;
+                   ch_state_d = CH_D; 
+                 end
+               end
+
+               CH_D: begin
+                 start_d=1'b0;
+                 if (!mems_SPI_busy && start_q == 1'b0) begin 
+                   addr_d = 4'd2;
+                   start_d = 1'b1;
+                   ch_state_d = CH_A; 
+                   // delay_cntr_d = 10'd1;
+                 end
+
+               end
+
+               default: ch_state_d = CH_A;
+             endcase
+
+      end // SET_CHANNEL
+
+      default: state_d = IDLE;
+    endcase
+  end
+ 
+  always @(posedge clk) begin
+    if (rst) begin
+      state_q <= IDLE;
+    end else begin
+      state_q <= state_d;
+      ch_state_q <= ch_state_d;
+    end
+
+    delta_A_q <= delta_A_d; 
+    delta_B_q <= delta_B_d; 
+    delta_C_q <= delta_C_d; 
+    delta_D_q <= delta_D_d; 
+    start_q<=start_d;
+    addr_q <= addr_d;
+    // delay_cntr_q <= delay_cntr_d;
+  end
+ 
+endmodule
